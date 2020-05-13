839db6f67d55fe1bd5ec411f7a3776c15690672f
Michael O'Keefe <mokeefe@mitre.org>
2020-05-13 08:33:11 -0400

    Add label for the profile selector

--- src/components/ProfileSelect.tsx
+++ src/components/ProfileSelect.tsx
@@ -4,12 +4,15 @@ import Select from 'react-select';
 export class ProfileSelect extends Component<any, {}> {
   render() {
     return (
-      <Select
-        isClearable
-        options={ this.props.options }
-        name="profile"
-        id="profile"
-      />
+      <div>
+        <label htmlFor="profile">Select a profile:</label>
+        <Select
+          isClearable
+          options={ this.props.options }
+          name="profile"
+          id="profile"
+        />
+      </div>
     );
   }
 }
