90a79a0a631345b21a256c96bb931139a268f586
Jason Crowley <crowleyj@mitre.org>
2020-06-04 16:16:16 -0400

    Add banner to show whether user input is valid JSON/resource and what resourceType was detected

--- src/components/ResourceForm.tsx
+++ src/components/ResourceForm.tsx
@@ -1,12 +1,44 @@
 import React, { useState } from 'react';
 
+interface Resource {
+  resourceType: string;
+}
+
+function isResource(o: any): o is Resource {
+  return o instanceof Object && o.hasOwnProperty('resourceType');
+}
+
+function parseResource(input: string): Resource {
+  let parsedJson;
+  try {
+    parsedJson = JSON.parse(input);
+  } catch (e) {
+    throw new Error('Invalid JSON');
+  }
+
+  if (isResource(parsedJson)) {
+    return parsedJson;
+  } else {
+    throw new Error('Missing "resourceType" field');
+  }
+}
+
 export function ResourceForm() {
   const [input, setInput] = useState('');
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => setInput(e.target.value);
 
+  let banner;
+  try {
+    const resource = parseResource(input);
+    banner = <div className="alert alert-success">Detected resource of type: {resource.resourceType}</div>
+  } catch (error) {
+    banner = <div className="alert alert-danger">{error.message}</div>
+  }
+
   return (
     <div className="form-group">
+      {banner}
       <label htmlFor="resource_field">Paste your FHIR resource here:</label>
       <textarea
         name="resource_field"
