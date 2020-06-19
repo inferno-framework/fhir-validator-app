aa5109011ea80c5405e551c676a73d03f06bac9e
Jason Crowley <crowleyj@mitre.org>
2020-06-19 17:01:42 -0400

    Fix webpack config to serve profiles.json and use HtmlWebpackPlugin for development and production

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,6 @@ const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const liveReload = require('webpack-livereload-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: "development",
@@ -10,6 +9,7 @@ module.exports = merge(common, {
   devServer: {
     port: 4567,
     historyApiFallback: true,
+    contentBase: path.join(__dirname, 'public'),
   },
 
   // Enable sourcemaps for debugging webpack's output.
@@ -19,9 +19,5 @@ module.exports = merge(common, {
 
   plugins: [
     new liveReload(),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public/index.html'),
-      filename: 'index.html',
-    }),
   ],
 });
