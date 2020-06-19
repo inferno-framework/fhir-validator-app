c83c974262269fde2817dac024f2348b3baaf1c4
Jason Crowley <crowleyj@mitre.org>
2020-06-19 15:58:43 -0400

    Install and use webpack-dev-server and html-webpack-plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,16 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const liveReload = require('webpack-livereload-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: "development",
+
+  devServer: {
+    port: 4567,
+    historyApiFallback: true,
+  },
 
   // Enable sourcemaps for debugging webpack's output.
   devtool: "source-map",
@@ -11,6 +18,10 @@ module.exports = merge(common, {
   watch: true,
 
   plugins: [
-    new liveReload()
+    new liveReload(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+      filename: 'index.html',
+    }),
   ],
 });
