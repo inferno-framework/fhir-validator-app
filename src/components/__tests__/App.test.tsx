ea22bb6df53e40bcfb27f025288f4e59925ce035
Jason Crowley <crowleyj@mitre.org>
2020-06-26 13:05:12 -0400

    Test that form state is cleared after an unmount

--- src/components/__tests__/App.test.tsx
+++ src/components/__tests__/App.test.tsx
@@ -16,13 +16,26 @@ describe('<App />', () => {
 
   it('redirects users who manually navigate to the results page back to the home page', async () => {
     const { queryByText } = renderWithRouter(<App />, { route: '/validate' });
-    expect(queryByText(/validated.*resource/i)).toBeFalsy();
-    await waitFor(() => {});
+    await waitFor(() => expect(queryByText(/validated.*resource/i)).toBeFalsy());
   });
 
   it.todo('shows a 404 page if the user visits an invalid route');
 
-  it('routes to results page on form submit and persist state when going back', async () => {
+  it('clears form state after an unmount', async () => {
+    const { getByLabelText: getBefore, unmount, history } = renderWithRouter(<App />);
+    const  textField = getBefore(/paste.*resource/i);
+
+    expect(textField).toHaveValue('');
+    fireEvent.change(textField, { target: { value: 'foobar' } });
+    expect(textField).toHaveValue('foobar');
+
+    // Clears form state after an unmount
+    expect(unmount()).toBe(true);
+    const { getByLabelText: getAfter } = renderWithRouter(<App />, { history });
+    await waitFor(() => expect(getAfter(/paste.*resource/i)).toHaveValue(''));
+  });
+
+  it('routes to results page on form submit and restores state when going back', async () => {
     const { getByLabelText, getByDisplayValue, queryByText, history } = renderWithRouter(<App />);
     const textField = getByLabelText(/paste.*resource/i);
     const submitButton = getByDisplayValue(/validate/i);
@@ -32,10 +45,11 @@ describe('<App />', () => {
     fireEvent.change(textField, { target: { value: '{ "resourceType": "Patient" }' } });
     fireEvent.click(submitButton);
 
+    // Reroutes to results page on form submit
     await waitFor(() => expect(queryByText(/validated.*resource/i)).toBeTruthy());
 
+    // Restores form state when going back
     history.goBack();
-
     await waitFor(() => expect(queryByText(/validated.*resource/i)).toBeFalsy());
     expect(getByLabelText(/paste.*resource/i)).toHaveValue('{ "resourceType": "Patient" }');
   });
