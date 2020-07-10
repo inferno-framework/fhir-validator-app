4f094c61af4742188e8892d78b5649a59222320c
Jason Crowley <crowleyj@mitre.org>
2020-07-10 09:13:10 -0400

    Add GuideSelect component

---
+++ src/components/GuideSelect.tsx
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import Select from 'react-select';
+
+import { SelectOption } from '../models/SelectOption';
+import { FormContext } from './ValidatorForm';
+
+export interface GuideSelectProps {
+  readonly igs?: string[];
+}
+
+export function GuideSelect({ igs }: GuideSelectProps) {
+  const [formState, dispatch] = useContext(FormContext);
+  const value = formState['implementation_guide'];
+  const handleChange = (value: SelectOption) => dispatch({ name: 'implementation_guide', value });
+  const options = igs?.map(ig => new SelectOption(ig, ig));
+
+  return (
+    <div>
+      <label htmlFor="implementation_guide">Select a profile:</label>
+      <Select
+        isClearable
+        isDisabled={!options}
+        options={options}
+        name="implementation_guide"
+        id="implementation_guide"
+        value={value}
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
