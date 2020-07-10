f0bfa4dba698a8837ef54a46928e3c10a7d53472
Jason Crowley <crowleyj@mitre.org>
2020-07-10 11:09:18 -0400

    Set default IG only after IGs are retrieved from the server

--- src/components/ProfileForm.tsx
+++ src/components/ProfileForm.tsx
@@ -11,18 +11,24 @@ export function ProfileForm() {
   const [igs, setIgs] = useState<string[]>();
   const [profiles, setProfiles] = useState<Record<string, string[]>>({});
 
-  const ig = formState['implementation_guide']?.value ?? 'hl7.fhir.r4.core';
+  const ig = formState['implementation_guide']?.value;
   const options = profiles[ig]?.map(profile => new SelectOption(profile, profile));
 
   useEffect(() => {
     let aborted = false;
-    getIgs().then(igs => !aborted && setIgs(Object.keys(igs).sort()));
+    getIgs().then(igs => {
+      if (!aborted) {
+        setIgs(Object.keys(igs).sort());
+        const value = new SelectOption('hl7.fhir.r4.core', 'hl7.fhir.r4.core');
+        dispatch({ name: 'implementation_guide', value });
+      }
+    });
     return () => aborted = true;
   }, []);
 
   useEffect(() => {
     let aborted = false;
-    if (!options) {
+    if (ig && !options) {
       loadIg(ig).then(urls => !aborted && setProfiles(profiles => ({ ...profiles, [ig]: urls })));
     }
     return () => aborted = true;
