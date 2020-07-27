3cc5604976396b246b128f87365e68ba2242bc77
Jason Crowley <crowleyj@mitre.org>
2020-07-27 12:17:38 -0400

    Use consistent spacing for webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -3,57 +3,57 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { EnvironmentPlugin } = require('webpack');
 
 module.exports = {
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".jsx", ".js"]
-    },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".jsx", ".js"]
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.ts(x?)$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: "ts-loader"
-                    }
-                ]
-            },
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                loader: "source-map-loader"
-            }
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
         ]
-    },
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  },
 
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'public/index.html'),
-        filename: 'index.html',
-      }),
-      new EnvironmentPlugin({
-        BASE_PATH: '',
-        VALIDATOR_URL: 'http://localhost:8080/',
-      }),
-    ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+      filename: 'index.html',
+    }),
+    new EnvironmentPlugin({
+      BASE_PATH: '',
+      VALIDATOR_URL: 'http://localhost:8080/',
+    }),
+  ],
 
-    output: {
-        path: path.resolve(__dirname, "./public/js/dist")
-    },
+  output: {
+    path: path.resolve(__dirname, "./public/js/dist")
+  },
 
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  },
 
-    optimization: {
-        usedExports: true
-    }
-    
+  optimization: {
+    usedExports: true
+  }
+
 };
