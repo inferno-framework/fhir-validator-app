3033a3093ed1b4b847e1ccd65bee23ac4758534b
Jason Crowley <crowleyj@mitre.org>
2020-07-31 15:14:24 -0400

    Write heredoc to stdout in ./create_config.sh

--- create_config.sh
+++ create_config.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-echo "CONFIG = {" > ./config.js
-echo "  validatorBasePath: \"$VALIDATOR_BASE_PATH\"," >> ./config.js
-echo "  externalValidatorUrl: \"$EXTERNAL_VALIDATOR_URL\"," >> ./config.js
-echo "};" >> ./config.js
+cat << EOF
+CONFIG = {
+  validatorBasePath: "$VALIDATOR_BASE_PATH",
+  externalValidatorUrl: "$EXTERNAL_VALIDATOR_URL",
+};
+EOF
--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 envsubst '${VALIDATOR_BASE_PATH}' <  /nginx.conf.template > /etc/nginx/conf.d/default.conf
-sh ./create_config.sh
-cp config.js /validator
+sh ./create_config.sh > /validator/config.js
 
 exec "$@"
