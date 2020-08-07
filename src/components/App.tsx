5529c2e49d8a749ffcebcaffc2c4ce96ead17936
Jason Crowley <crowleyj@mitre.org>
2020-08-07 12:37:16 -0400

    Add getVersion function and add modal to display version info

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,21 @@
-import React, { ReactElement } from 'react';
+import React, { useState, useEffect, ReactElement } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { ValidatorForm, FormState } from './ValidatorForm';
 import { Results, RESULTS_PATH } from './Results';
-import { ValidationResult } from 'models/HL7Validator';
+import { getVersion, ValidationResult } from 'models/HL7Validator';
 import config from 'utils/config';
 import appVersion from 'version';
 
 export type AppState = FormState & { results?: ValidationResult };
 
 export function App(): ReactElement {
+  const [validatorVersion, setVersion] = useState('');
+  useEffect(() => {
+    let aborted = false;
+    getVersion().then((version) => !aborted && setVersion(version));
+    return (): void => void (aborted = true);
+  }, []);
+
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -37,8 +44,36 @@ export function App(): ReactElement {
         >
           Issues
         </a>
-        <div className="navbar-text">Version {appVersion}</div>
+        <a className="navbar-link" href="#!" data-toggle="modal" data-target="#version-modal">
+          Version {appVersion}
+        </a>
       </nav>
+      <div
+        className="modal fade"
+        id="version-modal"
+        tabIndex={-1}
+        role="dialog"
+        aria-labelledby="version-modal-label"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="version-modal-label">
+                Version Info
+              </h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              FHIR Validator App Version: {appVersion}
+              <br />
+              FHIR Validation Service Version: {validatorVersion}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
