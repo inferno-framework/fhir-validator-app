bed00201911bc48a602f782135b8a7d4719a80d6
jason-crowley <44309688+jason-crowley@users.noreply.github.com>
2020-09-02 16:24:58 -0400

    Add and use Blob polyfill for reading files (#37)

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './components/App';
+import './polyfills';
 
 ReactDOM.render(
   <StrictMode>
